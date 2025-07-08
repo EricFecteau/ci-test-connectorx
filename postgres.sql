2f8e359af75b9c659d70f890135ae6d17ecfa7d4
Eric Fecteau <eric.m.fecteau@gmail.com>
2025-07-08 19:01:29 -0400

    Comma

--- postgres.sql
+++ postgres.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS test_types;
-DROP TABLE IF EXISTS test_infinite_values;
 
 CREATE TABLE IF NOT EXISTS test_types(
     test_timestamp TIMESTAMP,
-    test_timestamptz TIMESTAMPTZ,
+    test_timestamptz TIMESTAMPTZ
 );
 
 /*                              test_timestamp                  test_timestamptz               */
@@ -11,4 +10,4 @@ INSERT INTO test_types VALUES ( '1970-01-01 00:00:01',          '1970-01-01 00:0
 INSERT INTO test_types VALUES ( '2000-02-28 12:00:10',          '2000-02-28 12:00:10-04'       );
 INSERT INTO test_types VALUES ( '2038-01-18 23:59:59',          '2038-01-18 23:59:59+08'       );
 INSERT INTO test_types VALUES ( '1901-12-14 00:00:00.062547',   '1901-12-14 00:00:00.062547-12');
-INSERT INTO test_types VALUES ( NULL,                           NULL,                          );
+INSERT INTO test_types VALUES ( NULL,                           NULL                          );
