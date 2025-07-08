0678dbcb1346f63eafebea180dad30cfe61f06fd
Eric Fecteau <eric.m.fecteau@gmail.com>
2025-07-08 18:54:53 -0400

    Wrong URL

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import connectorx as cx
 import pyarrow
 
-postgres_url = "postgresql://postgres:postgres@localhost:5432"
+postgres_url = "postgresql://postgres:postgres@postgres:5432/postgres"
 query = "SELECT test_timestamptz FROM test_types"
 df = cx.read_sql(postgres_url, query, return_type="arrow")
 
