f33c13506ec6d2a956279dd77fe12ddf3d20260e
Eric Fecteau <eric.m.fecteau@gmail.com>
2025-07-08 18:52:06 -0400

    Add python test

---
+++ test.py
@@ -0,0 +1,11 @@
+import connectorx as cx
+import pyarrow
+
+postgres_url = "postgresql://postgres:postgres@localhost:5432"
+query = "SELECT test_timestamptz FROM test_types"
+df = cx.read_sql(postgres_url, query, return_type="arrow")
+
+print(df.column("test_timestamptz")[0])
+print(df.column("test_timestamptz")[1])
+print(df.column("test_timestamptz")[2])
+print(df.column("test_timestamptz")[3])
